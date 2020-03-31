5e9b25114088d7f302248376df8d63a3b2b128ec
fabian <fabmil22@gmail.com>
2020-03-31 21:44:03 +0200

    agregamos sass al proyecto  con  webpack

--- src/components/Helloword.jsx
+++ src/components/Helloword.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import '../assets/style/App.scss';
 
-const HelloWord = ()=> {
-    <h1>Hola mundo</h1>
-}
+const HelloWord = ()=> ( <h1>Hola mundo</h1>
+)
 
 export default HelloWord;
