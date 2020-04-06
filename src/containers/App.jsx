5cdb9da49b077a3ec107abe5dade93cf180e5cfc
Fmiliani <fmiliani@paradigmadigital>
2020-04-06 18:19:38 +0200

    CONECTADO CON REDUX

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React , { useEffect, useState} from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categorias from '../components/Categorias';
@@ -6,47 +6,48 @@ import Carrusel from '../components/Carrusel';
 import ItemCarrusel from '../components/ItemCarrusel';
 import Footer from '../components/Footer';
 import '../assets/style/App.scss';
-import useInitialState from '../hooks/useInitialState';
 
-const API = "http://localhost:3000/initalState";
+import { connect } from 'react-redux';
 
-const App = () => {
- const videoslista = useInitialState(API);
-    return ( 
-        <div  className="App">
-        <Header />
-        <Search />
-        {
-            videoslista.mylist?.length > 0 && 
-            <Categorias title="Nuevos">
-            <Carrusel>
-                <ItemCarrusel  lista={videoslista.trends} />
-            </Carrusel>
-            </Categorias>
-        }
-       
-            <Categorias title="tendencias">
-            <Carrusel>
-                { videoslista.trends.map(item =>
-                    <ItemCarrusel  key={item.id} {...item} />
-                    )}
-                
-            </Carrusel>
-            </Categorias>
-        
-            <Categorias title="nuevos">
-            <Carrusel>
-                { videoslista.originals.map(item =>
-                    <ItemCarrusel  key={item.id} {...item} />
-                    )}
-                
-            </Carrusel>
-            </Categorias>
+const App = ({ mylist, trends, originals }) => {
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {mylist?.length > 0 && (
+        <Categorias title='Nuevos'>
+          <Carrusel>
+            <ItemCarrusel lista={trends} />
+          </Carrusel>
+        </Categorias>
+      )}
 
-       
-        <Footer/>
-        </div>
-     );
-}
- 
-export default App;
+      <Categorias title='tendencias'>
+        <Carrusel>
+          {trends.map((item) => (
+            <ItemCarrusel key={item.id} {...item} />
+          ))}
+        </Carrusel>
+      </Categorias>
+
+      <Categorias title='nuevos'>
+        <Carrusel>
+          {originals.map((item) => (
+            <ItemCarrusel key={item.id} { ...item} />
+          ))}
+        </Carrusel>
+      </Categorias>
+
+      <Footer />
+    </div>
+  );
+};
+
+const mapStateToProsp = (state) => {
+  return {
+    mylist: state.mylist,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+export default connect(mapStateToProsp, null)(App);
